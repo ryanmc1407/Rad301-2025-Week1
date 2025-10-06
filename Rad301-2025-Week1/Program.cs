9a4f83ca2f2ff424e07abbbd521f2f3a6423a4df
Ryan Mc Clelland <S00236888@atu.ie>
2025-10-06 13:37:33 +0100

    Lab 1

--- Rad301-2025-Week1/Program.cs
+++ Rad301-2025-Week1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Rad301_2025_Week1;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ProductModel model = new ProductModel();
+    }
+}
